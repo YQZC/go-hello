95e8731f5dccb921b45fcb669acf2e3bf7652b5b
张超 <1765487360@qq.com>
2024-02-26 14:47:38 +0800

    release v1.0.0

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
-module github.com/YQZC/go-hello
+//module github.com/YQZC/go-hello
+
+module github.com/YQZC/go-hello/v1
 
 go 1.19
--- hello.go
+++ hello.go
@@ -2,6 +2,14 @@ package go_hello
 
 import "fmt"
 
-func SayHi() {
+//v0.1.0
+
+/*func SayHi() {
 	fmt.Println("你好，我是json, 很高兴认识你")
+}*/
+
+//v1.0.0
+
+func SayHi(name string) {
+	fmt.Printf("你好，我是%s, 很高兴认识你", name)
 }
